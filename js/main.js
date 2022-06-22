ef9e5246bfb2d107bbf23f2c7748b186ef969bb8
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-23 08:41:54 +0900

    v-onディレクティブ

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const app = Vue.createApp({
   data: () => ({
-    user: {
-      firstname: 'Taro',
-      lastname: 'Yamada',
-      age: 23
+    now: '-'
+  }),
+  methods: {
+    onClick: function() {
+      // console.log('onClick!')
+      // データオプションのnowプロパティを指す
+      this.now = new Date().toLocaleString()
+      // console.log(this)
     }
-  })
+  }
 })
 app.mount('#app')
