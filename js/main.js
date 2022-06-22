e1cd402c6198899c48aabb4f14cc8e707b6070c9
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-22 22:17:00 +0900

    属性のデータバインディング

--- js/main.js
+++ js/main.js
@@ -1,30 +1,6 @@
-// Vue.jsのバージョンをコンソールに出力
-console.log(Vue.version)
-
-// Vueインスタンスをappに格納
-// app
-// vm
-// const app = Vue.createApp({
-  // optionを記述
-  // データオブジェクトを定義
-  // data: () => ({
-    // message: 'Hello Vue.js3!'
-  // })
-// })
-// マウントする要素は、idがapp
-// Vueインスタンスのマウントメソッドを呼び出している
-// app.mount('#app')
-
 const app = Vue.createApp({
   data: () => ({
-    message: 'Hello Vue.js',
-    count: 99,
-    user: {
-      lastName: 'Nakamura',
-      firstName: 'Yuta',
-      prefecture: 'Tokyo'
-    },
-    colors: ['Red', 'Yellow', 'Blue']
+    message: 'Hello, Vue.js!'
   })
 })
 app.mount('#app')
