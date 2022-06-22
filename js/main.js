5f5f2fc5d236690de427d0fdd5277cf732911172
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-23 08:55:46 +0900

    コンポーネントを利用しよう

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
   data: () => ({
-    message: 'Hello, Vue.js!'
+   
   }),
   
+})
+app.component('hello-component', {
+  template: '<p>Hello!</p>'
 })
 app.mount('#app')
