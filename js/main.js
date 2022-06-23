0af9d0fd155a8e348eb0b850c682b04513d297a4
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-23 16:57:37 +0900

    v-onceディレクティブ

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
 const app = Vue.createApp({
   data: () => ({
-    newItem: '',
-    todos: []
+    message: 'Hello Vue.js!'
   }),
   methods: {
-    addItem: function(event) {
-      // console.log('Clicked!')
-      if(this.newItem === '') return
-      let todo = {
-        item: this.newItem,
-        isDone: false
-      }
-      this.todos.push(todo)
-      this.newItem = ""
-    },
-    deleteItem: function(index) {
-      // console.log('Delete!')
-      // console.log(index)
-      this.todos.splice(index, 1)
+    clickHandler: function(event) {
+      this.message =this.message.split('').reverse().join('')
     }
   }
 })
