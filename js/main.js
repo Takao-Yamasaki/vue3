da5b0798385b097fbc362948f894d6c408dcb26d
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-23 17:02:37 +0900

    v-preディレクティブ

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 const app = Vue.createApp({
   data: () => ({
     message: 'Hello Vue.js!'
-  }),
-  methods: {
-    clickHandler: function(event) {
-      this.message =this.message.split('').reverse().join('')
-    }
-  }
+  })
 })
 
 app.mount('#app')
